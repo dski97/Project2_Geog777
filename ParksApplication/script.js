86488c069c6a269d352c4d29e6135f76271273e6
Dominic <46698040+dski97@users.noreply.github.com>
2023-10-17 15:51:29 -0400

    updates

--- ParksApplication/script.js
+++ ParksApplication/script.js
@@ -3,7 +3,7 @@ require([
     "esri/Map",
     "esri/views/MapView"
 ], function (esriConfig, Map, MapView) {
-    esriConfig.apiKey = "YOUR_API_KEY_HERE";  // Replace with your API key
+    esriConfig.apiKey = "AAPK0401ef0eec41482c8aa0f4e7b5b118c24q7q7NIv8wbHpLPAB-Gvij3hfV_lpqWtqNzqcjDlMqJCmQc5meuAETYsSI2KOgu_";  // Replace with your API key
 
     var map = new Map({
         basemap: "arcgis-topographic"  // ArcGIS Online basemap
