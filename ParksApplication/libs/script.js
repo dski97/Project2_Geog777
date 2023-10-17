ffb9aadfec3b1c04868d3113c2b5cc9c96c62177
Dominic <46698040+dski97@users.noreply.github.com>
2023-10-17 15:25:56 -0400

    coding

---
+++ ParksApplication/libs/script.js
@@ -0,0 +1,15 @@
+require([
+    "esri/Map",
+    "esri/views/MapView"
+], function (Map, MapView) {
+    var map = new Map({
+        basemap: "topo-vector"
+    });
+
+    var view = new MapView({
+        container: "viewDiv",
+        map: map,
+        zoom: 4,
+        center: [-96, 37] // longitude, latitude (adjusted to center on the US)
+    });
+});
