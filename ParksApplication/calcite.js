11fd537d36ed752bb7c69f5bf4d05d31f8fe2441
Dominic <46698040+dski97@users.noreply.github.com>
2023-10-27 11:59:42 -0400

    UI edits

--- ParksApplication/calcite.js
+++ ParksApplication/calcite.js
@@ -1,6 +1,12 @@
 const button = document.getElementById("example-button");
 const modal = document.getElementById("example-modal");
+const doneButton = document.querySelector("calcite-button[slot='primary']");
 
 button?.addEventListener("click", function() {
     modal.open = true;
 });
+
+doneButton?.addEventListener("click", function() {
+    modal.open = false;
+});
+
