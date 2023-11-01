0e89a7f501f154f2832c170649ad7050a3745e69
Dominic <46698040+dski97@users.noreply.github.com>
2023-11-01 14:09:01 -0400

    changes to script

--- ParksApplication/calcite.js
+++ ParksApplication/calcite.js
@@ -1,6 +1,9 @@
 const button = document.getElementById("example-button");
 const modal = document.getElementById("example-modal");
 const doneButton = document.querySelector("calcite-button[slot='primary']");
+const openmodal = document.getElementById("opener");
+const navbar = document.getElementById("navLogo");
+const modalbutton = document.getElementById("calcite-button[slot='okay']");
 
 button?.addEventListener("click", function() {
     modal.open = true;
@@ -8,5 +11,13 @@ button?.addEventListener("click", function() {
 
 doneButton?.addEventListener("click", function() {
     modal.open = false;
+});
+
+navbar?.addEventListener("click", function() {
+    openmodal.open = true
+});
+
+modalbutton?.addEventListener("click", function() {
+    openmodal.open = false
 });
 
